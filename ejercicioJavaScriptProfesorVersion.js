4e24ccd5ad50dfb500a5bb6b4b59cb372b35eebd
Truemel <tgmelipilla@gmail.com>
2023-03-29 22:38:55 -0300

    Readme actualizado

---
+++ ejercicioJavaScriptProfesorVersion.js
@@ -0,0 +1,17 @@
+var masa = 10;
+var altura = 5;
+
+var IMC = masa/altura*altura;
+console.log(IMC);
+
+var compra = 3500;
+const IVA = 19;
+
+var valorIVACompra = compra*IVA/100;
+console.log(valorIVACompra);
+
+var opuesto = 5;
+var adyacente = 8;
+var hypotenusa = Math.sqrt((opuesto*opuesto)+(adyacente*adyacente));
+
+console.log(hypotenusa);
